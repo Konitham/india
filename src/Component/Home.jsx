0ed2d75fac933d346a92175d5f1eb1e93841187a
Konitham <95424818+Konitham@users.noreply.github.com>
2023-06-01 22:20:23 +0530

    Update Home.jsx

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,15 +1,1 @@
-import React from 'react'
-import Map from './Map'
-import Search from './Search'
 
-
-const Home = () => {
-  return (
-    <div>
-    <Search/>
-    <Map/>
-    </div>
-  )
-}
-
-export default Home
